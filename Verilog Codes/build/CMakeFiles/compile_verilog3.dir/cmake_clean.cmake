005ebf9c6598096956c23780c5312289cc01e3fa
achintyapro03 <achintyaharsha03@gmail.com>
2024-04-17 23:59:24 +0530

    Cmak made

---
+++ Verilog Codes/build/CMakeFiles/compile_verilog3.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compile_verilog3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/compile_verilog3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
