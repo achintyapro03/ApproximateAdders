bad04d366090d56d805f7449009906d11e0fb1b2
nitheezkant <85250569+nitheezkant@users.noreply.github.com>
2024-04-21 19:01:03 +0530

    ko

--- Verilog Codes/src/tb.v
+++ Verilog Codes/src/tb.v
@@ -23,73 +23,83 @@ nBitRcpa1 #(.N(N)) nBitRcpa1Inst(
     .A(A1),
     .B(B1),
     .sum(sum1),
-	.fn(fn1)
+    .fn(fn1)
 );
 
 nBitRcpa2 #(.N(N)) nBitRcpa2Inst(
     .A(A2),
     .B(B2),
     .sum(sum2),
-	.fn(fn2)
+    .fn(fn2)
 );
 
 nBitRcpa3 #(.N(N)) nBitRcpa3Inst(
     .A(A3),
     .B(B3),
     .sum(sum3),
-	.fn(fn3)
+    .fn(fn3)
 );
 
+// File operation variables
+reg [N-1:0] A, B;
+reg [N-1:0] sumActualFile, sum1File, sum2File, sum3File;
+reg [1:0] counter;
 
+// CSV file writing task
+task write_to_csv;
+    $fwrite(csv_file, "%d, %d, %d, %d, %d, %d, %d, %d \n", 
+        counter, A, B, sumActualFile, sum1File, sum2File, sum3File);
+endtask
+
+// Initial block to set up file and execute test cases
 initial begin
+    // Open CSV file for writing
+    csv_file = $fopen("output.csv", "w");
+
+    // Write CSV file header
+    $fwrite(csv_file, "Test, A, B, SumActual, Sum1, Sum2, Sum3\n");
+
+    // Set up dumpfile for waveform
     $dumpfile("exe/output_adder_comp.vcd");
     $dumpvars(0, tb);
 
-    // A = 4'b0011; B = 4'b0110; #10;
-    // A = 8'b01011110; B = 8'b00110010; #10;
-    // A = 1'b1; B = 1'b1; #10; 
+    // Execute test cases
+    counter = 0;
 
-	A1 = 8'b  110010; B1 = 8'b       1; 
-	A2 = 8'b  110010; B2 = 8'b       1;
-	A3 = 8'b  110010; B3 = 8'b       1; #10;
-	A1 = 8'b 1010001; B1 = 8'b   10010; 
-	A2 = 8'b 1010001; B2 = 8'b   10010;
-	A3 = 8'b 1010001; B3 = 8'b   10010; #10;
-	A1 = 8'b  111100; B1 = 8'b 1010001; 
-	A2 = 8'b  111100; B2 = 8'b 1010001;
-	A3 = 8'b  111100; B3 = 8'b 1010001; #10;
-	A1 = 8'b 1100111; B1 = 8'b 1100001; 
-	A2 = 8'b 1100111; B2 = 8'b 1100001;
-	A3 = 8'b 1100111; B3 = 8'b 1100001; #10;
-	A1 = 8'b 1001011; B1 = 8'b   10101; 
-	A2 = 8'b 1001011; B2 = 8'b   10101;
-	A3 = 8'b 1001011; B3 = 8'b   10101; #10;
-	A1 = 8'b    1011; B1 = 8'b   10111; 
-	A2 = 8'b    1011; B2 = 8'b   10111;
-	A3 = 8'b    1011; B3 = 8'b   10111; #10;
-	A1 = 8'b 1111101; B1 = 8'b  100001; 
-	A2 = 8'b 1111101; B2 = 8'b  100001;
-	A3 = 8'b 1111101; B3 = 8'b  100001; #10;
-	A1 = 8'b 1110010; B1 = 8'b 1000011; 
-	A2 = 8'b 1110010; B2 = 8'b 1000011;
-	A3 = 8'b 1110010; B3 = 8'b 1000011; #10;
-	A1 = 8'b   10000; B1 = 8'b  111000; 
-	A2 = 8'b   10000; B2 = 8'b  111000;
-	A3 = 8'b   10000; B3 = 8'b  111000; #10;
-	A1 = 8'b  100011; B1 = 8'b  110010; 
-	A2 = 8'b  100011; B2 = 8'b  110010;
-	A3 = 8'b  100011; B3 = 8'b  110010; #10;
-	A1 = 8'b  100101; B1 = 8'b    1000; 
-	A2 = 8'b  100101; B2 = 8'b    1000;
-	A3 = 8'b  100101; B3 = 8'b    1000; #10;
-	A1 = 8'b 1101010; B1 = 8'b 1001100; 
-	A2 = 8'b 1101010; B2 = 8'b 1001100;
-	A3 = 8'b 1101010; B3 = 8'b 1001100;
+   // repeat (1) begin
+        // Test case 1
+        A = 8'b110010; B = 8'b1;
+        A1 = A; B1 = B; A2 = A; B2 = B; A3 = A; B3 = B;
+        #10;
+        sumActualFile = A + B;
+        sum1File = sum1;
+        sum2File = sum2;
+        sum3File = sum3;
+        write_to_csv;
+        counter = counter + 1;
 
+        // Test case 2
+        A = 8'b1010001; B = 8'b10010;
+        A1 = A; B1 = B; A2 = A; B2 = B; A3 = A; B3 = B;
+        #10;
+        sumActualFile = A + B;
+        sum1File = sum1;
+        sum2File = sum2;
+        sum3File = sum3;
+        write_to_csv;
+        counter = counter + 1;
 
+        // Repeat for other test cases...
+  //  end
 
+    // Close CSV file
+    $fclose(csv_file);
 
+    // Finish simulation
     $finish;
 end
+
+// Declaration of CSV file handle
+integer csv_file;
 
 endmodule
